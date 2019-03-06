64a5f72ec3ae6dc3fa05568d0b96aaf4811b09db
Jim Disenstam <jim.disenstam@gmail.com>
2019-03-06 12:31:57 +0100

    Reworking page rendering with template tag

--- public/js/issues.js
+++ public/js/issues.js
@@ -6,22 +6,26 @@ async function getIssues () {
   let data = await fetch(`${apiURL}/issues`)
   data = await data.json()
   console.log(data)
+  $('#progressBar').remove()
   data.result.forEach(issue => {
-    $('#issuesCollection').append(
-      `<li class="collection-item avatar"><i class="material-icons circle">account_circle</i><span class="title">${issue.title}</span><p>Updated ${issue.updated_at} <br><a href="${issue.html_url}">Comments: ${issue.comments}</a></p><a href="#!" class="secondary-content"><i class="material-icons">grade</i></a></li>`
-    )
+    let li = document.querySelector('#issuesListTemplate').content.cloneNode(true)
+    li.querySelector('.title').textContent = issue.title
+    $('#issuesDiv ul').append(li)
   })
 }
-
-// getIssues()
 
 async function checkAuthorization () {
   console.log('running checkAuth')
   let data = await fetch(`${apiURL}/status`)
   data = await data.json()
   if (data.authorized) {
+    renderLoggedIn()
     getIssues()
   } else console.log('Not authorized')
+}
+
+function renderLoggedIn () {
+  $('#issuesDiv').append($('#issuesTemplate').html())
 }
 
 checkAuthorization()
